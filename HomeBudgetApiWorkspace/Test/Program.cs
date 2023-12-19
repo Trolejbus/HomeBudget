7f472fb9a4e1a3952ea333584be0834e690ef5d4
Michał Paduch (INI) <INI@kmd.dk>
2023-12-19 09:13:46 +0100

    wip

---
+++ HomeBudgetApiWorkspace/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using WebPush;
+
+Console.WriteLine("Hello, World!");
+
+
+string subscriptionUrl = @"https://fcm.googleapis.com/fcm/send/d_NvV0pEqNU:APA91bGU5AqwwTuMgU8sH_kKmDfwqMWSjsN-jtomZrKJh9ISMTaDr7FsMNwq5VowpJk1xFONZ6lFVyWSZ7CUW63JJbiAPw0kq-TbfumQKwwhhDy12xXER5J629ZPA-FOor6bMswBY3Vc";
+
+PushSubscription subscription = new PushSubscription(endpoint: subscriptionUrl, p256dh: "BOVyfRf031h8d3WcFCyiyBajxW-q4lmFAVV7kLJDT7LD6UbBLoJ2Nh3aVBMVWalaLlxIqf74_6wUQu0PdMXaO2Q", auth: "NzpRVw7-SiEdAT0xmXm9fg");
+string subject = "mailto:micha.duch@gmail.com";
+var vapidDetails = new VapidDetails(subject, publicKey: "BE-jdntMfI0JHJ-1hAZg4VnUCyiCJDOTB4qKOgErUoVG6l2kzTkR4ewKH6yLZg9g24LFeXQg8eSIHz8NhoTYY50", privateKey: "dpe7ndcmstKPvOUlAlDfyMUHjBnVPcugQOleH218SlA");
+var webPushClient = new WebPushClient();
+var message = JsonConvert.SerializeObject("Your product is ready");
+webPushClient.SendNotification(subscription, message, vapidDetails);
--- home-budget-workspace/projects/home-budget-mobile-app/src/app/app.component.ts
+++ home-budget-workspace/projects/home-budget-mobile-app/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'home-budget-mobile-app';
+  readonly VAPID_PUBLIC_KEY = "BE-jdntMfI0JHJ-1hAZg4VnUCyiCJDOTB4qKOgErUoVG6l2kzTkR4ewKH6yLZg9g24LFeXQg8eSIHz8NhoTYY50";
+
+  public error = '';
+  public test = '1';
+
+  constructor(
+    private swPush: SwPush,
+  ) {
+
+    this.swPush.messages.subscribe(m => console.log(m));
+  }
+
+  public sub(): void {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+    .then(sub => { console.log('test', sub.toJSON(), this.arrayBufferToBase64(sub.getKey("p256dh")), this.arrayBufferToBase64(sub.getKey("auth"))); this.test = '2'; })
+    .catch((err: Error) => { console.error("Could not subscribe to notifications", err); this.error = err.message; });
+  
+  }
+
+  private arrayBufferToBase64(buffer: ArrayBuffer | null): string {
+    if (buffer == null) {
+      throw new Error('Cannot be null');
+    }
+
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  };
 }
